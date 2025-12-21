d643c7233984c6ed85c7e605268aaf98a3268ae0
1272086709 <1272086709@qq.com>
2025-12-21 18:14:30 +0800

    feat: change components

--- app/auth/signin/page.js
+++ app/auth/signin/page.js
@@ -1,28 +1,20 @@
-//import { headers } from 'next/headers';
-import { signIn } from 'auth';
-export default async function SignIn() {
-  // const headersList = await headers();
-  // const host = headersList.get('host');
-  // const response = await fetch(`https://${host}/api/auth/csrf`);
-  // const {csrfToken} = await response.json()
+'use client';
+
+import { useEffect } from "react";
+
+export default function SignIn() {
+  const [csrfToken, setCsrfToken] = useState('');
+  useEffect(() => {
+    const fetchCsrfToken = async () => {
+      const response = await fetch(`/api/auth/csrf`);
+      const {csrfToken} = await response.json()
+      setCsrfToken(csrfToken)
+    };
+    fetchCsrfToken();
+  }, []);
   return (
-    <form method="post" action={async (formData) => {
-      'use server'; // 关键：声明这是一个 Server Action
-      try {
-        // 调用服务器端的 signIn 函数
-        await signIn('credentials', {
-          username: formData.get('username'),
-          password: formData.get('password'),
-          // 成功则重定向
-          redirectTo: '/',
-        });
-      } catch (error) {
-        // 这里可以更精细地处理错误，例如返回错误信息给页面
-        console.error('登录错误:', error);
-        // 可以根据错误类型重定向到带错误信息的页面
-        redirect('/auth/signin?error=CredentialsSignin');
-      }
-    }}>
+    <form method="post" action="/api/auth/callback/credentials">
+      <input type="hidden" name="csrfToken" value={csrfToken} />
       <label>
         Username
         <input name="username" type="text" />

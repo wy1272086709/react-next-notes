a577f5dd5649df27d7653e9d6743cbd45acd9410
1272086709 <1272086709@qq.com>
2025-12-21 17:55:22 +0800

    feat: commit codes

--- app/auth/signin/page.js
+++ app/auth/signin/page.js
@@ -1,9 +1,28 @@
-import { getCsrfToken } from 'next-auth/react';
-
+//import { headers } from 'next/headers';
+import { signIn } from 'auth';
 export default async function SignIn() {
-  const csrfToken = await getCsrfToken();
+  // const headersList = await headers();
+  // const host = headersList.get('host');
+  // const response = await fetch(`https://${host}/api/auth/csrf`);
+  // const {csrfToken} = await response.json()
   return (
-    <form method="post" action="/api/auth/callback/credentials">
+    <form method="post" action={async (formData) => {
+      'use server'; // 关键：声明这是一个 Server Action
+      try {
+        // 调用服务器端的 signIn 函数
+        await signIn('credentials', {
+          username: formData.get('username'),
+          password: formData.get('password'),
+          // 成功则重定向
+          redirectTo: '/',
+        });
+      } catch (error) {
+        // 这里可以更精细地处理错误，例如返回错误信息给页面
+        console.error('登录错误:', error);
+        // 可以根据错误类型重定向到带错误信息的页面
+        redirect('/auth/signin?error=CredentialsSignin');
+      }
+    }}>
       <input type="hidden" name="csrfToken" value={csrfToken} />
       <label>
         Username

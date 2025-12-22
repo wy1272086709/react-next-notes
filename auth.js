46013cd308f8f7666194cdbf2d3e6c2d21dd7b9f
1272086709 <1272086709@qq.com>
2025-12-22 22:36:33 +0800

    feat: commit codes

--- auth.js
+++ auth.js
@@ -1,48 +1,37 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { addUser, getUser } from "@/lib/prisma";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers:[CredentialsProvider({
-    // 显示按钮文案 (e.g. "Sign in with...")
-    name: "密码登录",
-    // `credentials` 用于渲染登录页面表单
-    credentials: {
-      username: { label: "账号", type: "text", placeholder: "输入您的账号" },
-      password: { label: "密码", type: "password", placeholder: "输入您的密码" }
-    },
-    // 处理从用户收到的认证信息
-    async authorize(credentials, req) {
-      // 默认情况下不对用户输入进行验证，确保使用 Zod 这样的库进行验证
-      let user = null
-
-      // 登陆信息验证
-      user = await getUser(credentials.username, credentials.password)
-
-      // 密码错误
-      if (user === 1) return null
-
-      // 用户注册
-      if (user === 0) {
-        user = await addUser(credentials.username, credentials.password)
-      }
-
-      if (!user) {
-        throw new Error("User was not found and could not be created.")
-      }
-
-      return user
-    }
-  })],
+const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "密码登录",
+      credentials: {
+        username: { label: "账号", type: "text", placeholder: "输入您的账号" },
+        password: { label: "密码", type: "password", placeholder: "输入您的密码" },
+      },
+      async authorize(credentials, req) {
+        let user = await getUser(credentials.username, credentials.password);
+        if (user === 1) return null;
+        if (user === 0) {
+          user = await addUser(credentials.username, credentials.password);
+        }
+        if (!user) {
+          throw new Error("User was not found and could not be created.");
+        }
+        return user;
+      },
+    }),
+  ],
   pages: {
-    signIn: '/auth/signin'
+    signIn: "/auth/signin",
   },
   callbacks: {
     authorized({ request, auth }) {
-      const { pathname } = request.nextUrl
-      if (pathname.startsWith("/note/edit")) return !!auth
-      return true
+      const { pathname } = request.nextUrl;
+      if (pathname.startsWith("/note/edit")) return !!auth;
+      return true;
     },
     async jwt({ token, user, account }) {
       if (account && account.type === "credentials" && user) {
@@ -53,7 +42,9 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     async session({ session, token }) {
       session.user.userId = token.userId;
       return session;
-    }
+    },
   },
-  trustHost: true
-})
+  trustHost: true,
+};
+
+export default NextAuth(authOptions);

82c9eb6741ccc92364a56e06dd660bf68dec65ab
1272086709 <1272086709@qq.com>
2026-01-17 18:01:20 +0800

    feat: 更改文件

--- i18n.js
+++ i18n.js
@@ -5,11 +5,14 @@ import {getRequestConfig} from 'next-intl/server';
 export const locales = ['zh', 'en'];
 export const defaultLocale = 'zh';
 
-export default getRequestConfig(async ({locale}) => {
+export default getRequestConfig(async ({requestLocale}) => {
   // 验证传入的 locale 参数
+  let locale = await requestLocale;
+
   if (!locales.includes(locale)) notFound();
 
   return {
+    locale,
     messages: (await import(`./messages/${locale}.json`)).default
   };
 });

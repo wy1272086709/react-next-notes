83fa5bb2ce22e62609b3290d263e8a3118d4941f
1272086709 <1272086709@qq.com>
2026-01-18 19:37:55 +0800

    feat: 更改文件

--- components/EditButton.js
+++ components/EditButton.js
@@ -2,12 +2,27 @@
 
 import Link from 'next/link'
 import {useLocale} from 'next-intl';
+import { useSession } from 'next-auth/react';
 
 export default function EditButton({noteId, children}) {
   const isDraft = noteId == null;
   const locale = useLocale();
+  const { status } = useSession();
+  const isAuthenticated = status === 'authenticated';
+
+  const handleClick = (e) => {
+    if (!isAuthenticated) {
+      e.preventDefault();
+      window.location.href = `/${locale}/login`;
+    }
+  };
+
   return (
-    <Link href={`/${locale}/note/edit/${noteId || ''}`} className="link--unstyled">
+    <Link
+      href={`/${locale}/note/edit/${noteId || ''}`}
+      className="link--unstyled"
+      onClick={handleClick}
+    >
       <button
         className={[
           'edit-button',

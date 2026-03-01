ba3fece9f3de9dc1a8dab782a3e6e1601b0844f9
1272086709 <1272086709@qq.com>
2026-03-01 12:22:41 +0800

    feat: commit codes

--- prisma/migrations/20240123062108_init/migration.sql
+++ prisma/migrations/20240123062108_init/migration.sql
@@ -10,8 +10,8 @@ CREATE TABLE `User` (
 -- CreateTable
 CREATE TABLE `Note` (
     `id` VARCHAR(191) NOT NULL,
-    `title` VARCHAR(255) NOT NULL,
-    `content` VARCHAR(191) NULL,
+    `title` TEXT NOT NULL,
+    `content` TEXT NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
     `authorId` VARCHAR(191) NOT NULL,
